cf8556d12152855a8fd9bd9c4f8e593b272ca127
Alexander Maxwell Adewole <alex@bizar.re>
2023-01-11 18:09:14 -0800

    Rename Musty::mongo -> Musty::new

--- musty/examples/mongo_custom_id_primitive.rs
+++ musty/examples/mongo_custom_id_primitive.rs
@@ -12,7 +12,7 @@ struct User {
 pub async fn main() -> musty::Result<()> {
     let client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
     let client = Client::with_options(client_options)?;
-    let db = Musty::mongo(client.database("musty"));
+    let db = Musty::new(client.database("musty"));
 
     // Insert a user into the collection
     let mut user = User { id: 1.into(), name: String::from("jonah") };
